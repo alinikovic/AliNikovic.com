49c2e5613e0b02915a6d8cd94d49468a7cea5598
Ali Nikovic <alinikovic@users.noreply.github.com>
2026-06-22 21:57:58 -0400

    Improve personal search visibility

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const mimeTypes = {
   ".css": "text/css; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".xml": "application/xml; charset=utf-8",
+  ".txt": "text/plain; charset=utf-8",
   ".svg": "image/svg+xml",
   ".png": "image/png",
   ".jpg": "image/jpeg",
